2e9e7c498c7552915696f5e829c973277d7ea680
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-05-10 02:16:50 +0530

    added

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "spoothy-bucket_name" {}
-variable "spoothy-bucket_tag_name" {}
-variable "spoothy-bucket_tag_env" {}
+variable "spoothy-bucket-tag_name" {}
+variable "spoothy-bucket-tag_env" {}
 variable "ami_id" {}
 variable "instance_type" {}
 
