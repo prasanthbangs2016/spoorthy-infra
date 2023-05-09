a867bd2014f33dd9f9054e00861d72a9f910ea8d
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-05-09 23:55:56 +0530

    added

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "s3" {
     source                 = "./vendor/modules/s3/"
-    bucket                 = "${var.spoothy_bucket_name}"
+    spoothy_bucket_tag_name = "${var.spoothy_bucket_tag_name}"
+    spoothy_bucket_tag_env  = "${var.spoothy_bucket_tag_env}"
 }
 
 module "ec2" {
